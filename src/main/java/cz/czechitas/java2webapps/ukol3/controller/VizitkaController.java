70b8d26791b582ad6f84be1bce6ac6137ee4f6a3
sonitko <sonitko.cz@gmail.com>
2021-04-22 19:53:38 +0200

    Úkol číslo 3

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,46 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
+@RequestMapping("/")
 public class VizitkaController {
-  @GetMapping("/")
-  public String seznam() {
-    return "seznam";
-  }
 
-  @GetMapping("/detail")
-  public String detail() {
-    return "detail";
-  }
+    private final List<Vizitka> vizitka;
+
+    public VizitkaController() {
+        vizitka = Arrays.asList(
+                new Vizitka("Tomáš Sokol", "Domácí zvířata", "Hrnčířská 120", "59501 Velká Bíteš", "sokol@nasezvire.cz", "+420 758833694", "www.nasezvire.cz"),
+                new Vizitka("Petr Ostrý", "Stavebniny s r.o.", "Velkopavlovická 21", "62800 Brno", "PetrOstry@stav.eu", null, "www.stavebniny.com"),
+                new Vizitka("Libuše Horáčková", "Nej Firma a.s.", "Požární 8", "36005 Karlovy Vary", null, "+420 451232154", "www.nejfirma.cz"),
+                new Vizitka("Stanislav Kutil", "Zahrada a.s.", "Kamenná 5", "35002 Cheb", null, "+420 236-859-566", null),
+                new Vizitka("Bořivoj Kudláček", "Albion a.s.", "Mostecká 123", null, "borek@albion.com", "+420 968-333-666", "www.albion.eu/borek"),
+                new Vizitka("Kateřina Krásná", "Hrady s r.o.", "hrad Bouzov", "78325 Bouzov", null, "+420 775 888 960", null)
+        );
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        ModelAndView modelAndView = new ModelAndView("seznam");
+        modelAndView.addObject("seznam", vizitka);
+        return modelAndView;
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detail(int id) {
+        ModelAndView modelAndView = new ModelAndView("detail");
+        modelAndView.addObject("detail", vizitka.get(id));
+        return modelAndView;
+
+    }
 }
---
+++ src/main/java/cz/czechitas/java2webapps/ukol3/entity/Vizitka.java
@@ -0,0 +1,88 @@
+package cz.czechitas.java2webapps.ukol3.entity;
+
+public class Vizitka {
+    private String jmeno;
+    private String firma;
+    private String ulice;
+    private String obecPsc;
+    private String email;
+    private String telefon;
+    private String web;
+    private String celaAdresa; // (readonly property, ve tvaru „ulice, obecPsc“)
+
+    public Vizitka() {
+    }
+
+    public Vizitka(String jmeno, String firma, String ulice, String obecPsc, String email, String telefon, String web) {
+        this.jmeno = jmeno;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.obecPsc = obecPsc;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+        if (ulice != null && obecPsc != null) {
+            this.celaAdresa = String.format("%s, %s", ulice, obecPsc);
+        }
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public String getObecPsc() {
+        return obecPsc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public String getCelaAdresa() {
+        return celaAdresa;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public void setObecPsc(String obecPsc) {
+        this.obecPsc = obecPsc;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+}
